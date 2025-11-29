8bcd91c6a9055d00d09fe637963eb49d38d4beed
R <robin.sachdeva@gmail.com>
2025-11-29 15:59:53 +0000

    refactor(carbonintensityapi): extract class field

--- api/carbonintensityapi/carbon_intensity.py
+++ api/carbonintensityapi/carbon_intensity.py
@@ -3,8 +3,10 @@ from datetime import datetime, timedelta, timezone
 
 
 class CarbonIntensity:
+    API_TIME_FORMAT: str = "%Y-%m-%dT%H:%MZ"  # ISO8601 format with 'Z' for UTC
+
     def __init__(self):
-        self.HEADERS = {'Accept': 'application/json'}
+        self.__HEADERS: dict = {'Accept': 'application/json'}
 
     @staticmethod
     def _minutes_to_iso8601_timestamp(minutes_from_now: float) -> str:
@@ -13,36 +15,29 @@ class CarbonIntensity:
         :param minutes_from_now: Number of minutes to add to current time
         :return: ISO8601 formatted timestamp string
         """
-        from datetime import datetime, timedelta, timezone
-
-        target = datetime.now(timezone.utc) + timedelta(minutes=minutes_from_now)
-        return target.strftime("%Y-%m-%dT%H:%MZ")
+        future = datetime.now(timezone.utc) + timedelta(minutes=minutes_from_now)
+        return future.strftime(CarbonIntensity.API_TIME_FORMAT)
 
     @staticmethod
-    def _hours_to_iso8601_timestamp(hours: float) -> str:
+    def _hours_to_iso8601_timestamp(hours_from_now: float) -> str:
         """
         Add specified minutes to the current time and concert to ISO8601 format.
-        :param hours: Number of hours to add to current time
+        :param hours_from_now: Number of hours to add to current time
         :return: ISO8601 formatted timestamp string
         """
-        # Add hours to current time UTC
         now = datetime.now(timezone.utc)
-        future = now + timedelta(hours=hours)
-
-        # Format as ISO8601 with a trailing Z
-        iso_str = future.strftime("%Y-%m-%dT%H:%MZ")
-
-        return iso_str
+        future = now + timedelta(hours=hours_from_now)
+        return future.strftime(CarbonIntensity.API_TIME_FORMAT)
 
     def getDataToday(self) -> dict:
         """
         Get Carbon Intensity data for today as JSON.
-        :return: JSON data for today's Carbon Intensity.
+        :return: Data for today's Carbon Intensity.
         """
         r = requests.get(
             'https://api.carbonintensity.org.uk/intensity',
             params={},
-            headers=self.HEADERS
+            headers=self.__HEADERS
         )
         return r.json()
 
@@ -51,14 +46,14 @@ class CarbonIntensity:
         Get Carbon Intensity statistics until a certain number of hours from now.
         :param hours_duration: Number of hours from now to get statistics until.
         :param hours_start: Number of hours from now to start getting statistics.
-        :return: JSON data for Carbon Intensity statistics.
+        :return: Data for Carbon Intensity statistics.
         """
         start_time = CarbonIntensity._hours_to_iso8601_timestamp(hours_start)
         end_time = CarbonIntensity._hours_to_iso8601_timestamp(hours_duration)
         r = requests.get(
             f'https://api.carbonintensity.org.uk/intensity/stats/{start_time}/{end_time}',
             params={},
-            headers=self.HEADERS
+            headers=self.__HEADERS
         )
         return r.json()
 
