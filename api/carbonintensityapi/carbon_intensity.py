8ae5d5a3ce23adae542393b39944e76b4e792860
R <robin.sachdeva@gmail.com>
2025-11-29 15:44:00 +0000

    feat(carbonintensityapi): method to get detailed stats
    - Add helper to convert from hours into the future to iso8601
    - Add method to get data from the carbon intensity api based on how many
      hours in the future you want to see

--- api/carbonintensityapi/carbon_intensity.py
+++ api/carbonintensityapi/carbon_intensity.py
@@ -1,16 +1,52 @@
 import requests
+from datetime import datetime, timedelta, timezone
+
 
 class CarbonIntensity:
     def __init__(self):
         self.HEADERS = {'Accept': 'application/json'}
 
-    """
-    Get Carbon Intensity data for today.
-    """
-    def getDataToday(self):
+    @staticmethod
+    def _hours_to_iso8601_timestamp(hours: int) -> str:
+        """
+        Convert current time plus 24 hours to ISO8601 format.
+        """
+        # Add hours to current time UTC
+        now = datetime.now(timezone.utc)
+        future = now + timedelta(hours=hours)
+
+        # Format as ISO8601 with a trailing Z
+        iso_str = future.strftime("%Y-%m-%dT%H:%MZ")
+
+        return iso_str
+
+    def getDataToday(self) -> dict:
+        """
+        Get Carbon Intensity data for today as JSON.
+        """
         r = requests.get(
             'https://api.carbonintensity.org.uk/intensity',
             params={},
             headers=self.HEADERS
         )
         return r.json()
+
+    def getIntensityStatsUntil(self, hours: int) -> dict:
+        """
+        Get Carbon Intensity statistics until a certain number of hours from now.
+        """
+        start_time = CarbonIntensity._hours_to_iso8601_timestamp(0)
+        end_time = CarbonIntensity._hours_to_iso8601_timestamp(hours)
+        r = requests.get(
+            f'https://api.carbonintensity.org.uk/intensity/stats/{start_time}/{end_time}',
+            params={},
+            headers=self.HEADERS
+        )
+        return r.json()
+
+if __name__ == "__main__":
+    ci = CarbonIntensity()
+    data = ci.getDataToday()
+    print(data)
+    stats = ci.getIntensityStatsUntil(24)
+    print(stats)
