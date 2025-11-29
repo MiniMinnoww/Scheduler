2138d82488a2dba70bf5b0f19ecc6419bf295285
R <robin.sachdeva@gmail.com>
2025-11-29 15:52:11 +0000

    refactor(carbonintensityapi): make helper static

--- api/carbonintensityapi/carbon_intensity.py
+++ api/carbonintensityapi/carbon_intensity.py
@@ -7,7 +7,7 @@ class CarbonIntensity:
         self.HEADERS = {'Accept': 'application/json'}
 
     @staticmethod
-    def _hours_to_iso8601_timestamp(hours: int) -> str:
+    def _hours_to_iso8601_timestamp(hours: float) -> str:
         """
         Convert current time plus 24 hours to ISO8601 format.
         """
@@ -31,18 +31,21 @@ class CarbonIntensity:
         )
         return r.json()
 
-    def getIntensityStatsUntil(self, hours: int) -> dict:
+    def getIntensityStatsUntil(self, hours_duration: float, hours_start: float = 0) -> dict:
         """
         Get Carbon Intensity statistics until a certain number of hours from now.
+        :param hours_duration: Number of hours from now to get statistics until.
+        :param hours_start: Number of hours from now to start getting statistics.
         """
-        start_time = CarbonIntensity._hours_to_iso8601_timestamp(0)
-        end_time = CarbonIntensity._hours_to_iso8601_timestamp(hours)
+        start_time = CarbonIntensity._hours_to_iso8601_timestamp(hours_start)
+        end_time = CarbonIntensity._hours_to_iso8601_timestamp(hours_duration)
         r = requests.get(
             f'https://api.carbonintensity.org.uk/intensity/stats/{start_time}/{end_time}',
             params={},
             headers=self.HEADERS
         )
         return r.json()
+
 
 if __name__ == "__main__":
     ci = CarbonIntensity()
