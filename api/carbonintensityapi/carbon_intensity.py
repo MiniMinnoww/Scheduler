d70bc9a2ea7fe5fa22c2d63d5fb619d422ae99eb
R <robin.sachdeva@gmail.com>
2025-11-29 17:16:31 +0000

    feat: add getIntensityDataForHalfHour

--- api/carbonintensityapi/carbon_intensity.py
+++ api/carbonintensityapi/carbon_intensity.py
@@ -46,6 +46,33 @@ class CarbonIntensity:
         )
         return r.json()
 
+    def getIntensityDataForHalfHour(self, date: str, half_hour_settlement: int) -> dict:
+        """
+        Get Carbon Intensity data for a specific half hour period of a certain date.
+        :param date: Date in YYYY-MM-DD format.
+        :param half_hour_settlement: Half hour settlement period between 1-48 (1 is the first settlement on this date)>
+        :return: Carbon intensity data for this period.
+        """
+        # Ensure date is string in format YYYY-MM-DD
+        try:
+            datetime.strptime(date, "%Y-%m-%d")
+        except ValueError:
+            raise ValueError("Date not string in YYYY-MM-DD format.")
+
+        # Ensure half hour chosen is integer in interval [1, 48]
+        if (not isinstance(half_hour_settlement, int) 
+            or 1 > half_hour_settlement 
+            or half_hour_settlement > 48
+        ):
+            raise ValueError("Half hour period chosen must be integer interval [1, 48]")
+
+        r = requests.get(
+            f'https://api.carbonintensity.org.uk/intensity/date/{date}/{half_hour_settlement}',
+            params={},
+            headers=self.__HEADERS
+        )
+        return r.json()
+
     def getIntensityStatsUntil(self, hours_duration: float, hours_start: float = 0) -> dict:
         """
         Get Carbon Intensity statistics until a certain number of hours from now.
@@ -65,6 +92,7 @@ class CarbonIntensity:
 
 if __name__ == "__main__":
     ci = CarbonIntensity()
-    t = ci.getIntensityStatsUntil(0.25)
+    t = ci.getIntensityDataForHalfHour("2025-11-29", 1)
     print(t)
-    print(t['data'][0]['intensity'])
+    t = ci.getIntensityDataForHalfHour("2025-11-29", 2)
+    print(t)
